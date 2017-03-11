5ed530d1001cd23f0d6abd86493028a5d36b2053
Edmund Li <edmund.xz.lee@gmail.com>
2017-03-11 13:09:03 -0800

    garbage collection benchmark

--- benchmark/each_slice_bang_bm.rb
+++ benchmark/each_slice_bang_bm.rb
@@ -1,18 +1,53 @@
 require 'benchmark'
 require_relative '../lib/hyper_iterator'
 
-GC.disable
+class Thing; end
 
 puts '---------------------------------------------------------'
 puts '---------------------- each_slice! ----------------------'
-puts '---------------------------------------------------------'
-puts '--------------------- Memory Usage ----------------------'
 
-class Thing; end
+puts '---------------------------------------------------------'
+puts '------------------ Garbage Collection -------------------'
+
+slize_size = rand(1..10)
+
+puts 'Array#slice'
+puts '----------------------'
+
+arr = Array.new(100) { Thing.new }
+line = ''
+
+arr.each_slice(slize_size) do |slice|
+  GC.start
+  print ObjectSpace.each_object(Thing).count
+  print ' '
+end
+
+puts
+puts
+
+puts 'Array#slice!'
+puts '----------------------'
+
+arr = Array.new(100) { Thing.new }
+
+arr.each_slice!(slize_size) do |slice|
+  GC.start
+  print ObjectSpace.each_object(Thing).count
+  print ' '
+end
+
+puts
+
+puts '---------------------------------------------------------'
+puts '-------------------- Objects Created --------------------'
+
+
+GC.disable
 arr = Array.new(1000) { Thing.new }
 
 before = ObjectSpace.count_objects
-arr.each_slice(2) do |slice|
+arr.each_slice(slize_size) do |slice|
 end
 after = ObjectSpace.count_objects
 
@@ -26,7 +61,7 @@ puts
 arr = Array.new(1000) { Thing.new }
 
 before = ObjectSpace.count_objects
-arr.each_slice!(2) do |slice|
+arr.each_slice!(slize_size) do |slice|
 end
 after = ObjectSpace.count_objects
 
@@ -36,7 +71,6 @@ puts "# of arrays: %d" % (after[:T_ARRAY] - before[:T_ARRAY])
 puts "# of nodes: %d" % (after[:T_NODE] - before[:T_NODE])
 
 puts '---------------------------------------------------------'
-
 puts '--------------- Execution Time Comparison ---------------'
 
 n = 50000
