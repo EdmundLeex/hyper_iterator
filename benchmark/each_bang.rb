697b6f76940803925402246747bd627d9bc018e7
Edmund Li <edmund.xz.lee@gmail.com>
2017-03-16 10:46:47 -0700

    change report format

--- benchmark/each_bang.rb
+++ benchmark/each_bang.rb
@@ -1,3 +1,4 @@
+require 'benchmark'
 require_relative '../lib/hyper_iterator'
 $stdout.sync = true
 
@@ -12,9 +13,11 @@ i = 0
 while true
   print "#{i} "
   arr = []
-  arrs[i].each! do |el|
-    arr << el.dup
+  report = Benchmark.measure do
+    arrs[i].each! { |el| arr << el.dup }
   end
+
+  puts report
   arrs << arr
   i += 1
 end
